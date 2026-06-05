017685cb23a529a196f662af7fe936f3bf563c45
zhuchen <1606812180@qq.com>
2026-06-05 21:13:11 +0800

    feat:完成todo-listhtml页面

---
+++ 1.todo-list/script.js
@@ -0,0 +1,9 @@
+let confirmBtn = document.getElementById('confirm-btn');
+
+confirmBtn.addEventListener("click", ev => {
+    const toDoItem = document.getElementById('todo-input');
+    const toDoValue = toDoItem.value;
+    let str = '您输入的值是: '
+    console.log(`${str}${toDoValue}`);
+    toDoItem.value = '';
+})
